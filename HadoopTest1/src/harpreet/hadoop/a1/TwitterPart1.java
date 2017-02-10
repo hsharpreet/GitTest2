4d6368b075ca5dd38bf2908609c2b66db408ce91
Harpreet <Harpreet@192.168.2.10>
2017-02-10 16:29:36 -0500

    Output from first map and reducer is perfect, it combines the required opposite values output for twitter and negative values from input list.

--- HadoopTest1/src/harpreet/hadoop/a1/TwitterPart1.java
+++ HadoopTest1/src/harpreet/hadoop/a1/TwitterPart1.java
@@ -12,6 +12,7 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.Reducer.Context;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
@@ -21,32 +22,6 @@ public class TwitterPart1 {
 
         public void map(Object key, Text values, Context context) throws IOException, InterruptedException {
             StringTokenizer st = new StringTokenizer(values.toString());
-            /*System.out.println("=====");
-            System.out.print("st.countTokens()-"+st.countTokens()+" , ");
-            
-            while (st.hasMoreTokens()) {
-            	System.out.print("/"+Integer.parseInt(st.nextToken()));
-            }
-            System.out.println("=====");
-            
-            =====
-    		st.countTokens()-4 , /1/3/4/5=====
-    		=====
-    		st.countTokens()-4 , /2/1/3/5=====
-    		=====
-    		st.countTokens()-5 , /3/1/2/4/5=====
-    		=====
-    		st.countTokens()-5 , /4/1/2/3/5=====
-    		=====
-    		st.countTokens()-2 , /5/3=====
-    		
-    		ACTUAL
-    		1	2(2) 
-			2	4(1) 
-			3	
-			4	
-			5	1(3) 2(2) 4(2) 
-    		*/
             
             
             IntWritable user = new IntWritable(Integer.parseInt(st.nextToken()));
@@ -56,17 +31,25 @@ public class TwitterPart1 {
             // (user,-friend)
             // 'friend1' will be used in the emitted pair
             IntWritable friend1 = new IntWritable();
+            IntWritable friend2 = new IntWritable();
             while (st.hasMoreTokens()) {
-                Integer friend = Integer.parseInt(st.nextToken());
+                /*Integer friend = Integer.parseInt(st.nextToken());
                 friend1.set(friend);
                 //context.write(user, friend1);
                 context.write(friend1, user);
+                friends.add(friend); // save the friends of user 'user' for later*/
+            
+            	Integer friend = Integer.parseInt(st.nextToken());
+                friend1.set(-friend);
+                friend2.set(friend);
+                context.write(user, friend1);
+                context.write(friend2, user);
                 friends.add(friend); // save the friends of user 'user' for later
             }
             // Now we can emit all (a,b) and (b,a) pairs
             // where a!=b and a & b are friends of user 'user'.
             // We use the same algorithm as before.
-            ArrayList<Integer> seenFriends = new ArrayList<>();
+            /*ArrayList<Integer> seenFriends = new ArrayList<>();
             // The element in the pairs that will be emitted.
             IntWritable friend2 = new IntWritable();
             for (Integer friend : friends) {
@@ -77,11 +60,156 @@ public class TwitterPart1 {
                   //  context.write(friend2, friend1);
                 }
                 seenFriends.add(friend1.get());
-            }
+            }*/
         }
     }
 
     public static class CountReducer extends Reducer<IntWritable, IntWritable, IntWritable, Text> {
+
+        // A private class to describe a recommendation.
+        // A recommendation has a friend id and a number of friends in common.
+        private static class Recommendation {
+
+            // Attributes
+            private int friendId;
+            private int nCommonFriends;
+
+            // Constructor
+            public Recommendation(int friendId) {
+                this.friendId = friendId;
+                // A recommendation must have at least 1 common friend
+                this.nCommonFriends = 1;
+            }
+
+            // Getters
+            public int getFriendId() {
+                return friendId;
+            }
+
+            /*public int getNCommonFriends() {
+                return nCommonFriends;
+            }
+
+            // Other methods
+            // Increments the number of common friends
+            public void addCommonFriend() {
+                nCommonFriends++;
+            }*/
+
+            // String representation used in the reduce output            
+            public String toString() {
+                return friendId + ""; 
+                /*intentionally commented
+                "(" + nCommonFriends + ")";*/
+            }
+
+            // Finds a representation in an array
+            public static Recommendation find(int friendId, ArrayList<Recommendation> recommendations) {
+                for (Recommendation p : recommendations) {
+                    if (p.getFriendId() == friendId) {
+                        return p;
+                    }
+                }
+                // Recommendation was not found!
+                return null;
+            }
+        }
+
+        // The reduce method       
+        public void reduce(IntWritable key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+            IntWritable user = key;
+            // 'existingFriends' will store the friends of user 'user'
+            // (the negative values in 'values').
+            ArrayList<Integer> existingFriends = new ArrayList();
+            // 'recommendedUsers' will store the list of user ids recommended
+            // to user 'user'
+            ArrayList<Integer> allUsers = new ArrayList<>();
+            ArrayList<Integer> recommendedUsersOld = new ArrayList<>();
+            while (values.iterator().hasNext()) {
+                int value = values.iterator().next().get();
+                allUsers.add(value);
+            }
+            // 'recommendedUsers' now contains all the positive values in 'values'.
+            // We need to remove from it every value -x where x is in existingFriends.
+            // See javadoc on Predicate: https://docs.oracle.com/javase/8/docs/api/java/util/function/Predicate.html
+            /*for (Integer friend : existingFriends) {
+                recommendedUsers.removeIf(new Predicate<Integer>() {
+                    @Override
+                    public boolean test(Integer t) {
+                        return t.intValue() == -friend.intValue();
+                    }
+                });
+            }*/
+            ArrayList<Recommendation> recommendations = new ArrayList<>();
+            // Builds the recommendation array
+            for (Integer userId : allUsers) {
+                Recommendation p = Recommendation.find(userId, recommendations);
+                if (p == null) {
+                    recommendations.add(new Recommendation(userId));
+                } //intentionally commented 
+                /*else {
+                    p.addCommonFriend();
+                }*/
+            }
+            // Sorts the recommendation array
+            // See javadoc on Comparator at https://docs.oracle.com/javase/8/docs/api/java/util/Comparator.html
+            /*recommendations.sort(new Comparator<Recommendation>() {
+                @Override
+                public int compare(Recommendation t, Recommendation t1) {
+                    return -Integer.compare(t.getNCommonFriends(), t1.getNCommonFriends());
+                }
+            });*/
+            // Builds the output string that will be emitted
+            StringBuffer sb = new StringBuffer(""); // Using a StringBuffer is more efficient than concatenating strings
+            for (int i = 0; i < recommendations.size() && i < 10; i++) {
+                Recommendation p = recommendations.get(i);
+                sb.append(p.toString() + " ");
+            }
+            Text result = new Text(sb.toString());
+            context.write(user, result);
+        }
+    }
+    
+    public static class AllPairsMapper2 extends Mapper<Object, Text, IntWritable, IntWritable> {
+
+        public void map(Object key, Text values, Context context) throws IOException, InterruptedException {
+            StringTokenizer st = new StringTokenizer(values.toString());
+            IntWritable user = new IntWritable(Integer.parseInt(st.nextToken()));
+
+            // 'friends' will store the list of friends of user 'user'
+            ArrayList<Integer> friends = new ArrayList<>();
+            
+            // First, go through the list of all friends of user 'user' and emit 
+            // (user,-friend)
+            // 'friend1' will be used in the emitted pair
+            IntWritable friend1 = new IntWritable();
+            while (st.hasMoreTokens()) {
+                Integer friend = Integer.parseInt(st.nextToken());
+                friend1.set(-friend);
+                context.write(user, friend1);
+                System.out.println("user: "+user+"\t friend: "+friend);
+                friends.add(friend); // save the friends of user 'user' for later
+            }
+            
+            // Now we can emit all (a,b) and (b,a) pairs
+            // where a!=b and a & b are friends of user 'user'.
+            // We use the same algorithm as before.
+            ArrayList<Integer> seenFriends = new ArrayList<>();
+            // The element in the pairs that will be emitted.
+            IntWritable friend2 = new IntWritable();
+            for (Integer friend : friends) {
+                friend1.set(friend);
+                for (Integer seenFriend : seenFriends) {
+                    friend2.set(seenFriend);
+                    context.write(friend1, friend2);
+                    context.write(friend2, friend1);
+                }
+                seenFriends.add(friend1.get());
+            }
+        }
+    }
+
+    public static class CountReducer2 extends Reducer<IntWritable, IntWritable, IntWritable, Text> {
 
         // A private class to describe a recommendation.
         // A recommendation has a friend id and a number of friends in common.
@@ -188,16 +316,35 @@ public class TwitterPart1 {
     }
 
     public static void main(String[] args) throws IOException, InterruptedException, ClassNotFoundException {
+    	
+    	String inputDir = "/Users/Harpreet/gitTest2/HadoopTest1/input";
+    	String outputTempDir = "/Users/Harpreet/gitTest2/HadoopTest1/output/temp1";
+    	String outputFinalDir = "/Users/Harpreet/gitTest2/HadoopTest1/output/final1";
+
         Configuration conf = new Configuration();
-        Job job = Job.getInstance(conf, "people you may know");
-        job.setJarByClass(TwitterPart1.class);
-        job.setMapperClass(AllPairsMapper.class);
-        job.setReducerClass(CountReducer.class);
-        job.setOutputKeyClass(IntWritable.class);
-        job.setOutputValueClass(IntWritable.class);
-        FileInputFormat.addInputPath(job, new Path(args[0]));
-        FileOutputFormat.setOutputPath(job, new Path(args[1]));
-        System.exit(job.waitForCompletion(true) ? 0 : 1);
+        Job job1 = Job.getInstance(conf, "twitter1");
+        job1.setJarByClass(TwitterPart1.class);
+        job1.setMapperClass(AllPairsMapper.class);
+        job1.setReducerClass(CountReducer.class);
+        job1.setOutputKeyClass(IntWritable.class);
+        job1.setOutputValueClass(IntWritable.class);
+        FileInputFormat.addInputPath(job1, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job1, new Path(outputTempDir));
+      //  System.exit(job1.waitForCompletion(true) ? 0 : 1);
+        
+        boolean success = job1.waitForCompletion(true);
+        if (success) {
+	        //Configuration conf2 = new Configuration();
+	        Job job2 = Job.getInstance(conf, "twitter2");
+	        job2.setJarByClass(TwitterPart1.class);
+	        job2.setMapperClass(AllPairsMapper2.class);
+	       // job2.setReducerClass(CountReducer2.class);
+	        job2.setOutputKeyClass(IntWritable.class);
+	        job2.setOutputValueClass(IntWritable.class);
+	        FileInputFormat.addInputPath(job2, new Path(outputTempDir));
+	        FileOutputFormat.setOutputPath(job2, new Path(outputFinalDir));
+	        System.exit(job2.waitForCompletion(true) ? 0 : 1);
+        }
     }
 
 }
