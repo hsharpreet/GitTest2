089cd79b657627cf4bd5190b6051446b36c338c9
Harpreet <Harpreet@192.168.2.10>
2017-02-10 17:31:53 -0500

    Output of Map 2 is good here. Combinations of (xy), (yx) and (user,exisitingFrnd) and emitted

--- HadoopTest1/src/harpreet/hadoop/a1/TwitterPart1.java
+++ HadoopTest1/src/harpreet/hadoop/a1/TwitterPart1.java
@@ -46,21 +46,7 @@ public class TwitterPart1 {
                 context.write(friend2, user);
                 friends.add(friend); // save the friends of user 'user' for later
             }
-            // Now we can emit all (a,b) and (b,a) pairs
-            // where a!=b and a & b are friends of user 'user'.
-            // We use the same algorithm as before.
-            /*ArrayList<Integer> seenFriends = new ArrayList<>();
-            // The element in the pairs that will be emitted.
-            IntWritable friend2 = new IntWritable();
-            for (Integer friend : friends) {
-                friend1.set(friend);
-                for (Integer seenFriend : seenFriends) {
-                    friend2.set(seenFriend);
-                  //  context.write(friend1, friend2);
-                  //  context.write(friend2, friend1);
-                }
-                seenFriends.add(friend1.get());
-            }*/
+           
         }
     }
 
@@ -177,34 +163,39 @@ public class TwitterPart1 {
             IntWritable user = new IntWritable(Integer.parseInt(st.nextToken()));
 
             // 'friends' will store the list of friends of user 'user'
-            ArrayList<Integer> friends = new ArrayList<>();
-            
+            ArrayList<Integer> pFriends = new ArrayList<>();
+            ArrayList<Integer> nFriends = new ArrayList<>();
             // First, go through the list of all friends of user 'user' and emit 
             // (user,-friend)
             // 'friend1' will be used in the emitted pair
-            IntWritable friend1 = new IntWritable();
             while (st.hasMoreTokens()) {
                 Integer friend = Integer.parseInt(st.nextToken());
-                friend1.set(-friend);
-                context.write(user, friend1);
-                System.out.println("user: "+user+"\t friend: "+friend);
-                friends.add(friend); // save the friends of user 'user' for later
-            }
-            
-            // Now we can emit all (a,b) and (b,a) pairs
-            // where a!=b and a & b are friends of user 'user'.
-            // We use the same algorithm as before.
-            ArrayList<Integer> seenFriends = new ArrayList<>();
-            // The element in the pairs that will be emitted.
-            IntWritable friend2 = new IntWritable();
-            for (Integer friend : friends) {
-                friend1.set(friend);
-                for (Integer seenFriend : seenFriends) {
-                    friend2.set(seenFriend);
-                    context.write(friend1, friend2);
-                    context.write(friend2, friend1);
+                if(friend>0){
+                	pFriends.add(friend);
+                }else{
+                	nFriends.add(friend);
                 }
-                seenFriends.add(friend1.get());
+            }
+            IntWritable frnd1 = new IntWritable();
+            IntWritable frnd2 = new IntWritable();
+            
+            if(pFriends.size()>0){
+                for (Integer tempFriend1 : pFriends) {
+                	frnd1.set(tempFriend1);
+                	for (Integer tempFriend2 : pFriends) {
+                    	frnd2.set(tempFriend2);
+                    	if(tempFriend1 != tempFriend2){
+                        	context.write(frnd1, frnd2);
+                            //context.write(frnd2,frnd1);
+                    	}
+                	}
+                }
+            }
+            if(nFriends.size()>0){
+                for (Integer friend : nFriends) {
+                	frnd2.set(friend);
+                	context.write(user, frnd2);
+                }
             }
         }
     }
@@ -275,6 +266,8 @@ public class TwitterPart1 {
                     existingFriends.add(value);
                 }
             }
+            System.out.println(recommendedUsers);
+            System.out.println(existingFriends);
             // 'recommendedUsers' now contains all the positive values in 'values'.
             // We need to remove from it every value -x where x is in existingFriends.
             // See javadoc on Predicate: https://docs.oracle.com/javase/8/docs/api/java/util/function/Predicate.html
@@ -318,8 +311,8 @@ public class TwitterPart1 {
     public static void main(String[] args) throws IOException, InterruptedException, ClassNotFoundException {
     	
     	String inputDir = "/Users/Harpreet/gitTest2/HadoopTest1/input";
-    	String outputTempDir = "/Users/Harpreet/gitTest2/HadoopTest1/output/temp1";
-    	String outputFinalDir = "/Users/Harpreet/gitTest2/HadoopTest1/output/final1";
+    	String outputTempDir = "/Users/Harpreet/gitTest2/HadoopTest1/output/temp2";
+    	String outputFinalDir = "/Users/Harpreet/gitTest2/HadoopTest1/output/final2";
 
         Configuration conf = new Configuration();
         Job job1 = Job.getInstance(conf, "twitter1");
